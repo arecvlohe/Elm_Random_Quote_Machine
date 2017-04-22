42b3713d0e7342ed774eff3cb11f319af64c9061
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-04-21 23:52:19 -0400

    Fetching quotes from a random quote api

--- Main.elm
+++ Main.elm
@@ -2,62 +2,115 @@ module Main exposing (..)
 
 import Html exposing (Html, div, button, text)
 import Html.Events exposing (onClick)
-import Random
-import List.Extra exposing (getAt)
+import Html.Attributes exposing (disabled)
 import Maybe exposing (withDefault)
+import Http exposing (Request, request, header, emptyBody, expectJson)
+import Json.Decode exposing (string, map2, at)
+
+
+{- import List.Extra exposing (getAt) -}
+-- REQUEST
+
+
+post : Request Quote
+post =
+    request
+        { method = "GET"
+        , headers =
+            [ header "X-TheySaidSo-Api-Secret" "l7GYREuEF11EJiJYl5zTCweF"
+            , header "Accept" "application/json"
+            ]
+        , url = "http://quotes.rest/quote/random.json"
+        , body = emptyBody
+        , expect = expectJson decoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+
+
+-- DECODER
+
+
+decoder : Json.Decode.Decoder Quote
+decoder =
+    map2 Quote (at [ "contents", "author" ] string) (at [ "contents", "quote" ] string)
+
 
 
 -- MODEL
 
 
-type alias Model =
+type alias Quote =
     { author : String
     , quote : String
     }
 
 
+type alias Model =
+    { content : Quote
+    , fetching : Bool
+    }
+
+
 model : Model
 model =
-    { author = "", quote = "" }
+    { content = { author = "", quote = "" }, fetching = False }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { author = "Adam", quote = "All things" }, Cmd.none )
-
-
-defaultQuote : Model
-defaultQuote =
-    { author = "Default author", quote = "Default quote" }
-
-
-quotes : List Model
-quotes =
-    [ { author = "Adam", quote = "Some things" }
-    , { author = "Joe", quote = "Some other things" }
-    , { author = "John", quote = "Some other other things" }
-    , { author = "Jacob", quote = "Good things" }
-    ]
+    ( { content = { author = "", quote = "" }, fetching = False }, getNewQuote )
 
 
 
+{-
+   defaultQuote : Model
+   defaultQuote =
+       { author = "Default author", quote = "Default quote" }
+
+
+   quotes : List Model
+   quotes =
+       [ { author = "Adam", quote = "Some things" }
+       , { author = "Joe", quote = "Some other things" }
+       , { author = "John", quote = "Some other other things" }
+       , { author = "Jacob", quote = "Good things" }
+       ]
+-}
 -- UPDATE
 
 
 type Msg
     = NoOp
     | FetchQuote
-    | RandomNumber Int
+    | NewQuote (Result Http.Error Quote)
+
+
+
+-- | RandomNumber Int
+
+
+getNewQuote : Cmd Msg
+getNewQuote =
+    Http.send NewQuote post
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         FetchQuote ->
-            ( model, Random.generate RandomNumber (Random.int 0 3) )
+            ( { model | fetching = True }, getNewQuote )
 
-        RandomNumber int ->
-            ( withDefault defaultQuote (getAt int quotes), Cmd.none )
+        {-
+           RandomNumber int ->
+               ( withDefault defaultQuote (getAt int quotes), Cmd.none )
+        -}
+        NewQuote (Ok response) ->
+            ( { model | content = response, fetching = False }, Cmd.none )
+
+        NewQuote (Err _) ->
+            ( { model | fetching = False }, Cmd.none )
 
         NoOp ->
             ( model, Cmd.none )
@@ -75,9 +128,9 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ div [] [ text model.author ]
-        , div [] [ text model.quote ]
-        , button [ onClick FetchQuote ] [ text "Fetch new quote" ]
+        [ div [] [ text model.content.author ]
+        , div [] [ text model.content.quote ]
+        , button [ onClick FetchQuote, disabled model.fetching ] [ text "Fetch new quote" ]
         ]
 
 
